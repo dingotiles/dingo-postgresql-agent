2400c2e0af9b89bbcf8c488cb9e30173d72c207a
Dr Nic Williams <drnicwilliams@gmail.com>
2016-10-26 14:26:04 +1000

    create /tmp

--- ci/tasks/delete-docker-processes.sh
+++ ci/tasks/delete-docker-processes.sh
@@ -3,9 +3,11 @@
 set -e -u
 
 DOCKER_VERSION=1.12.2
+mkdir /tmp/
 curl -L https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}.tgz -o /tmp/docker-${DOCKER_VERSION}.tgz
 tar xfz /tmp/docker-${DOCKER_VERSION}.tgz /tmp/
 mv /tmp/docker/docker /usr/local/bin/
+rm -rf /tmp/docker
 
 docker ps -a
 
