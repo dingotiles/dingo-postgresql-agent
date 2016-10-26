776d421dfac047f921f84e60dd9b102140d1f23f
Dr Nic Williams <drnicwilliams@gmail.com>
2016-10-26 14:25:01 +1000

    install docker cli

--- ci/tasks/delete-docker-processes.sh
+++ ci/tasks/delete-docker-processes.sh
@@ -2,6 +2,11 @@
 
 set -e -u
 
+DOCKER_VERSION=1.12.2
+curl -L https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}.tgz -o /tmp/docker-${DOCKER_VERSION}.tgz
+tar xfz /tmp/docker-${DOCKER_VERSION}.tgz /tmp/
+mv /tmp/docker/docker /usr/local/bin/
+
 docker ps -a
 
 docker ps -a | awk '{print $1}' | xargs -L1 docker rm -f
