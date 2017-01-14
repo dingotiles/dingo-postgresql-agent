f40ac98e51bf0319fc65676c2f222193ee219f94
Dr Nic Williams <drnicwilliams@gmail.com>
2017-01-13 21:36:23 -0800

    do not try to setup etcd auth if no password provided

--- images/scripts/initialize_etcd_auth.sh
+++ images/scripts/initialize_etcd_auth.sh
@@ -4,18 +4,23 @@ set -e
 
 # This script is used by tests to setup authorization user on etcd
 
-if [[ "$(curl -s ${ETCD_HOST_PORT:?required}/v2/auth/users |  jq -r .message)" != "Insufficient credentials" ]]; then
-  if [[ "$(curl -s ${ETCD_HOST_PORT:?required}/v2/auth/users |  jq -r .users)" == "null" ]]; then
-    echo "Creating missing root user..."
-    curl -s ${ETCD_HOST_PORT:?required}/v2/auth/users/root -X PUT -d "{\"user\":\"${ETCD_USERNAME:-root}\",\"password\":\"${ETCD_PASSWORD:?required}\"}"
+if [[ "${ETCD_PASSWORD}X" != "X" ]]; then
+  if [[ "$(curl -s ${ETCD_HOST_PORT:?required}/v2/auth/users |  jq -r .message)" != "Insufficient credentials" ]]; then
+    if [[ "$(curl -s ${ETCD_HOST_PORT:?required}/v2/auth/users |  jq -r .users)" == "null" ]]; then
+      echo "Creating missing root user..."
+      curl -s ${ETCD_HOST_PORT:?required}/v2/auth/users/root -X PUT -d "{\"user\":\"${ETCD_USERNAME:-root}\",\"password\":\"${ETCD_PASSWORD:?required}\"}"
+    fi
+
+    auth_enabled=$(curl -s ${ETCD_HOST_PORT:?required}/v2/auth/enable | jq -r .enabled)
+    if [[ "${auth_enabled}" != "true" ]]; then
+      echo "Enabling etcd authorization..."
+      curl -s ${ETCD_HOST_PORT:?required}/v2/auth/enable -X PUT
+    fi
   fi
 
-  auth_enabled=$(curl -s ${ETCD_HOST_PORT:?required}/v2/auth/enable | jq -r .enabled)
-  if [[ "${auth_enabled}" != "true" ]]; then
-    echo "Enabling etcd authorization..."
-    curl -s ${ETCD_HOST_PORT:?required}/v2/auth/enable -X PUT
-  fi
+  echo "Verifying crdentials..."
+  curl -s -u ${ETCD_USERNAME:-root}:${ETCD_PASSWORD:?required} ${ETCD_HOST_PORT}/v2/auth/users
+else
+  echo "Verifying that no credentials are required..."
+  curl -s ${ETCD_HOST_PORT}/v2/auth/enable
 fi
-
-echo "Verifying crdentials..."
-curl -s -u ${ETCD_USERNAME:-root}:${ETCD_PASSWORD:?required} ${ETCD_HOST_PORT}/v2/auth/users
