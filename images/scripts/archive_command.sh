2dcf0058f1f0bf341ee43bd23227abda70efd682
Dr Nic Williams <drnicwilliams@gmail.com>
2017-01-23 21:03:48 +1000

    use env vars passed into script

--- images/scripts/archive_command.sh
+++ images/scripts/archive_command.sh
@@ -11,11 +11,11 @@
 
 set -e -u
 
-path_name_of_file_to_archive=$1
-file_name=$2
+path_name_of_file_to_archive=$1 # %p
+file_name=$2                    # %f
 
 if [[ -d /etc/wal-e.d/env ]]; then
-  envdir /etc/wal-e.d/env wal-e wal-push \"%p\" -p 1
+  envdir /etc/wal-e.d/env wal-e wal-push "$path_name_of_file_to_archive" -p 1
 elif [[ "${RSYNC_HOSTNAME:-X}" != "X" ]]; then
   rsync -a $path_name_of_file_to_archive ${RSYNC_USERNAME}@${RSYNC_HOSTNAME}:${RSYNC_DEST_DIR}/wal_archive/$file_name
 else
--- images/scripts/restore_command.sh
+++ images/scripts/restore_command.sh
@@ -15,7 +15,7 @@ path_name_of_file_to_archive=$1 # %p
 file_name=$2                    # %f
 
 if [[ -d /etc/wal-e.d/env ]]; then
-  envdir /etc/wal-e.d/env wal-e wal-fetch \"%f\" \"${}\" -p 1
+  envdir /etc/wal-e.d/env wal-e wal-fetch "${file_name}" \"${}\" -p 1
 elif [[ "${RSYNC_HOSTNAME:-X}" != "X" ]]; then
   rsync -a ${RSYNC_USERNAME}@${RSYNC_HOSTNAME}:${RSYNC_DEST_DIR}/wal_archive/$file_name $path_name_of_file_to_archive
 else
