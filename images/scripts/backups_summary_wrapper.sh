4d6ab438c311edff10854d764bbbf0d8747a6881
Dr Nic Williams <drnicwilliams@gmail.com>
2017-03-03 11:37:59 +1000

    sanity check that /wale-backup-list is setup by backup-summary

--- images/scripts/backups_summary_wrapper.sh
+++ images/scripts/backups_summary_wrapper.sh
@@ -10,6 +10,7 @@ indent() {
   esac
 }
 
+STARTUP_POLL_WAITTIME=${STARTUP_POLL_WAITTIME:-1}
 BACKUPS_SUMMARY_WAITTIME=${BACKUPS_SUMMARY_WAITTIME:-60}
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -28,17 +29,23 @@ function wait_for_config {
   done
 }
 
+source ${patroni_env}
+echo ETCD_CLUSTER_URI=${ETCD_CLUSTER_URI:?required}
+
+backups_found=
 function backups_summary {
-  curl -s ${ETCD_CLUSTER_URI:?required}/wale-backup-list \
-    -X PUT -d "value=$(wal-e backup-list 2>/dev/null)" > /dev/null
+  curl -s ${ETCD_CLUSTER_URI}/wale-backup-list \
+    -X PUT -d "value=$(wal-e backup-list 2>/dev/null)" # > /dev/null
   backup_lines=$(wal-e backup-list 2>/dev/null | wc -l)
   if [[ $backup_lines -ge 2 ]]; then
     if [[ "${DEBUG:-}X" != "X" ]]; then
       echo "INFO: Backup status:"
       wal-e backup-list 2>/dev/null
     fi
+    backups_found=1
   else
     echo "WARNING: No backups successful yet"
+    backups_found=
   fi
 }
 
@@ -54,11 +61,22 @@ function backups_summary {
   fi
 
   while true; do
-    sleep ${BACKUPS_SUMMARY_WAITTIME}
-    pg_isready >/dev/null 2>&2 || continue
+    pg_isready >/dev/null 2>&2 || {
+      echo "database is not yet ready for connections: pg_isready"
+      sleep ${STARTUP_POLL_WAITTIME}
+      continue
+    }
     in_recovery=$(psql -U postgres -tqAc "select pg_is_in_recovery()")
-    if [[ "${in_recovery}" == "f" ]]; then
-      backups_summary
+    if [[ "${in_recovery}" != "f" ]]; then
+      echo "database is still in recovery: select pg_is_in_recovery()"
+      sleep ${STARTUP_POLL_WAITTIME}
+      continue
+    fi
+    backups_summary
+    if [[ "${backups_found}" == "1" ]]; then
+      sleep ${BACKUPS_SUMMARY_WAITTIME}
+    else
+      sleep ${STARTUP_POLL_WAITTIME}
     fi
   done
 ) 2>&1 | indent
