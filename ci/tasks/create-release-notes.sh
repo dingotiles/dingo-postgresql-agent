cdeb0b795ea8bbd6491b8e9a2fe2395c3bf73974
Dr Nic Williams <drnicwilliams@gmail.com>
2017-03-21 09:55:25 +1000

    future release notes include madlib reference [ci skip]

--- ci/tasks/create-release-notes.sh
+++ ci/tasks/create-release-notes.sh
@@ -13,10 +13,23 @@ cat > $NOTES/notes.md <<EOF
 
 TODO
 
-## Upgrade
+## Installation
+
+Normal image (with [Postgis](http://postgis.net/) extension support):
 
 \`\`\`
 docker pull dingotiles/dingo-postgresql:latest
 docker pull dingotiles/dingo-postgresql:v${version}
+
+docker run dingotiles/dingo-postgresql
+\`\`\`
+
+Image with [Madlib](https://madlib.incubator.apache.org/) extension support:
+
+\`\`\`
+docker pull dingotiles/dingo-postgresql-madlib:latest
+docker pull dingotiles/dingo-postgresql-madlib:v${version}
+
+docker run dingotiles/dingo-postgresql-madlib
 \`\`\`
 EOF
