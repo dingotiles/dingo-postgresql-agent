85f637acc3b452fd0442394f4c80342c9904b3bd
Dr Nic Williams <drnicwilliams@gmail.com>
2017-03-21 09:42:39 +1000

    shipit publishes madlib image

--- ci/tasks/dockerfile-versioned.sh
+++ ci/tasks/dockerfile-versioned.sh
@@ -4,8 +4,9 @@ set -e -u
 
 version=$(cat version/number)
 digest=$(cat image/digest)
+: ${image_name:?required}
 
 cat > dockerfile/Dockerfile << EOF
-FROM dingotiles/dingo-postgresql@${digest}
+FROM ${image_name}@${digest}
 ENV DINGO_IMAGE_VERSION=${version}
 EOF
